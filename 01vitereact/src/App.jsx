d79dd3c283e871958fc0037c7bbbc6e365e130ba
natalian07 <dsouzanatalian.com>
2024-07-14 18:49:34 +0530

    update

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
+
+
+import Chai from "./chai"
+
 
 function App() {
-
-  let [counter, setCounter] = useState(0)
-
-  // let counter=5
-
-  let addValue = () => {
-    // counter=counter + 1
-    setCounter(counter+1)
-    console.log("Clicked", counter);
-  }
-
-  let removeValue = () => {
-    setCounter(counter-1)
-  }
+  const username = "chai aur code"
 
   return (
     <>
-      <h1>Hello, React! | Natalian Dsouza</h1>
-      <h2>Counter value : {counter}</h2>
-      <button
-      onClick={addValue}
-      >Add value</button>
-      <br />
-      <br />
-      <button
-      onClick={removeValue}
-      >Remove value</button>
-    </>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
   )
 }
 
